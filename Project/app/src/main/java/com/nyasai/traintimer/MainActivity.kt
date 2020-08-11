a910bf42281207ea360689360b99ddc7d2da5eec
natsuyasai <natsuyasai7777@gmail.com>
2020-08-11 09:32:48 +0900

    プロジェクト新規作成

--- Project/app/src/main/java/com/nyasai/traintimer/MainActivity.kt
+++ Project/app/src/main/java/com/nyasai/traintimer/MainActivity.kt
@@ -2,21 +2,10 @@ package com.nyasai.traintimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.databinding.DataBindingUtil
-import com.nyasai.traintimer.ui.main.RouteListFragment
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, RouteListFragment.newInstance())
-                .commitNow()
-        }
     }
-
-
 }
