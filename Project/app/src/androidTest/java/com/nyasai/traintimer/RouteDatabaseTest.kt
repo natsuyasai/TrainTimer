ec9fa7f76c2efe6e14f7e6fcd8bcbf67f18b20ad
natsuyasai <natsuyasai7777@gmail.com>
2020-08-12 15:49:15 +0900

    クエリ作成 動作確認コード作成

---
+++ Project/app/src/androidTest/java/com/nyasai/traintimer/RouteDatabaseTest.kt
@@ -0,0 +1,64 @@
+package com.nyasai.traintimer
+
+import android.util.Log
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.nyasai.traintimer.database.RouteDatabase
+import com.nyasai.traintimer.database.RouteDatabaseDao
+import com.nyasai.traintimer.database.RouteListItem
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+import java.lang.Exception
+
+@RunWith(AndroidJUnit4::class)
+class RouteDatabaseTest {
+
+    private lateinit var routeDao: RouteDatabaseDao
+    private lateinit var db: RouteDatabase
+
+    // regiuon DB準備，片付け
+    @Before
+    fun createDB() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        db = Room.inMemoryDatabaseBuilder(context, RouteDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        routeDao = db.routeDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDB() {
+        db.close()
+    }
+
+    // endregiuon DB準備，片付け
+
+
+    // region テスト
+
+    @Test
+    @Throws(Exception::class)
+    fun insertRouteListItem() {
+        val item = RouteListItem()
+        item.routeName = "JR 宝塚線"
+        item.stationName = "草野駅"
+        item.direction = "大阪方面"
+        routeDao.insertRouteListItem(item)
+        val dbItems = routeDao.getAllRouteListItemsSync()
+        if(dbItems != null){
+            Log.d("DBTest", dbItems.toString())
+        }
+        else{
+            Log.d("DBTest", "null")
+        }
+        routeDao.clearAllRouteListItem()
+        //assertNull(dbItem)
+    }
+
+    // endregion
+}
--- Project/app/src/main/java/com/nyasai/traintimer/database/RouteDatabaseDao.kt
+++ Project/app/src/main/java/com/nyasai/traintimer/database/RouteDatabaseDao.kt
@@ -1,15 +1,127 @@
 package com.nyasai.traintimer.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface RouteDatabaseDao {
 
-    @Insert
-    fun insert(routeListItem: RouteListItem)
 
-    @Query("SELECT * from route_list_item_table WHERE dataId = :key")
-    fun get(key: Long): RouteListItem?
+    // region 路線リストアイテム操作
+
+    /**
+     * 路線アイテム追加
+     * @param routeListItem 追加アイテム
+     */
+    @Insert
+    fun insertRouteListItem(routeListItem: RouteListItem)
+
+    /**
+     * 路線アイテム更新
+     * @param routeListItem 更新アイテム
+     */
+    @Update
+    fun updateRouteListItem(routeListItem: RouteListItem)
+
+    /**
+     * 路線アイテム削除
+     */
+    @Query("DELETE FROM route_list_item_table WHERE dataId = :id")
+    fun deleteRouteListItem(id: Long)
+
+    /**
+     * 路線アイテム全クリア
+     */
+    @Query("DELETE FROM route_list_item_table")
+    fun clearAllRouteListItem()
+
+    /**
+     * 路線アイテム取得
+     * @param id 路線データID
+     */
+    @Query("SELECT * from route_list_item_table WHERE dataId = :id")
+    fun getRouteListItemWithId(id: Long): RouteListItem?
+
+    /**
+     * 路線アイテム一覧取得
+     */
+    @Query("SELECT * from route_list_item_table ORDER BY dataId DESC")
+    fun getAllRouteListItems(): LiveData<List<RouteListItem>>
+
+    /**
+     * 路線アイテム一覧取得(同期)
+     */
+    @Query("SELECT * from route_list_item_table ORDER BY dataId DESC")
+    fun getAllRouteListItemsSync(): List<RouteListItem>
+
+    // endregion 路線リストアイテム操作
+
+
+    // region 路線情報詳細操作
+
+    /**
+     * 路線詳細アイテム追加
+     * @param routeDetails 追加アイテム
+     */
+    @Insert
+    fun insertRouteDetailsItem(routeDetails: RouteDetails)
+
+    /**
+     * 路線詳細アイテム更新
+     * @param routeDetails 更新アイテム
+     */
+    @Update
+    fun updateRouteDetailsItem(routeDetails: RouteDetails)
+    @Update
+    fun updateRouteDetailsItems(routeDetails: List<RouteDetails>)
+
+    /**
+     * 路線詳細アイテム削除
+     */
+    @Query("DELETE FROM route_details_table WHERE dataId = :id")
+    fun deleteRouteDetailsItem(id: Long)
+
+    /**
+     * 路線詳細アイテム全クリア
+     */
+    @Query("DELETE FROM route_details_table")
+    fun clearAllRouteDetailsItem()
+
+    /**
+     * 路線詳細アイテム取得
+     * @param id 路線データID
+     */
+    @Query("SELECT * from route_details_table WHERE dataId = :id")
+    fun getRouteDetailsItemWithId(id: Long): RouteDetails?
+
+    /**
+     * 路線詳細アイテム一覧取得(親アイテムID指定)
+     * @param parentId 親アイテムID
+     */
+    @Query("SELECT * from route_details_table WHERE parent_row_id = :parentId ORDER BY dataId DESC")
+    fun getRouteDetailsItemsWithParentId(parentId: Long): LiveData<List<RouteDetails>>
+
+    /**
+     * 路線詳細アイテム一覧取得(親アイテムID指定)同期
+     * @param parentId 親アイテムID
+     */
+    @Query("SELECT * from route_details_table WHERE parent_row_id = :parentId ORDER BY dataId DESC")
+    fun getRouteDetailsItemsWithParentIdSync(parentId: Long): List<RouteDetails>
+
+    /**
+     * 路線詳細アイテム一覧取得
+     */
+    @Query("SELECT * from route_details_table ORDER BY dataId DESC")
+    fun getAllRouteDetailsItems(): LiveData<List<RouteDetails>>
+
+    /**
+     * 路線詳細アイテム一覧取得(同期)
+     */
+    @Query("SELECT * from route_details_table ORDER BY dataId DESC")
+    fun getAllRouteDetailsItemsSync(): List<RouteDetails>
+
+    // endregion 路線情報詳細操作
 }
