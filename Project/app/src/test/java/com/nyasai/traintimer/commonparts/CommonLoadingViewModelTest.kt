080ec2eab80694c6b7adbaa18df8f706bf3a5bf0
natsuyasai <yasai038tomato@gmail.com>
2021-09-26 18:11:55 +0900

    ローディングVMテスト作成

--- Project/app/src/test/java/com/nyasai/traintimer/commonparts/CommonLoadingViewModelTest.kt
+++ Project/app/src/test/java/com/nyasai/traintimer/commonparts/CommonLoadingViewModelTest.kt
@@ -32,6 +32,24 @@ internal class CommonLoadingViewModelTest{
     }
 
     /**
+     * ローディング表示_成功_テキスト未変更
+     */
+    @Test
+    fun showLoading_success_no_change_text() {
+        val target = CommonLoadingViewModel()
+        target.changeText("")
+
+        // オブザーバ設定
+        val observer = mock<Observer<String>>()
+        target.loadingText.observeForever(observer)
+
+        target.showLoading()
+
+        Assert.assertEquals(target.isVisible(), true)
+        verify(observer).onChanged("読み込み中")
+    }
+
+    /**
      * ローディング終了_成功
      */
     @Test
@@ -56,4 +74,42 @@ internal class CommonLoadingViewModelTest{
         Assert.assertEquals(maxCountObserver.get(), 0)
         Assert.assertEquals(currentCountObserver.get(), 0)
     }
+
+    /**
+     * 最大件数更新_成功
+     */
+    @Test
+    fun updateMaxCountFromBackgroundTask_success() {
+        val target = CommonLoadingViewModel()
+
+        // オブザーバ設定
+        val maxCountObserver = TestObserver<Int>()
+        target.maxCount.observeForever(maxCountObserver)
+
+        target.updateMaxCountFromBackgroundTask(100)
+
+        maxCountObserver.await()
+
+        Assert.assertEquals(maxCountObserver.get(), 100)
+    }
+
+    /**
+     * 最大件数インクリメント_成功
+     */
+    @Test
+    fun incrementMaxCountFromBackgroundTask_success() {
+        val target = CommonLoadingViewModel()
+
+        // オブザーバ設定
+        val maxCountObserver = TestObserver<Int>()
+        target.maxCount.observeForever(maxCountObserver)
+
+        target.incrementMaxCountFromBackgroundTask(1)
+
+        maxCountObserver.await()
+
+        Assert.assertEquals(maxCountObserver.get(), 1)
+    }
+
+
 }
